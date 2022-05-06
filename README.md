a836c60b6f168d86966f2cb145df284c35d0dba6
pakxe <pigkill40@naver.com>
2022-05-06 19:56:54 +0900

    [박세현] week2

--- README.md
+++ README.md
@@ -24,8 +24,8 @@ ex) <br>
 <img width="538" alt="image" src="https://user-images.githubusercontent.com/77886826/166138795-c60b1f82-9cd7-47fb-835a-04c6e0a4b02b.png"><br>
 현재 상태입니다.<br>
 <img width="663" alt="image" src="https://user-images.githubusercontent.com/77886826/166138831-708166b3-5251-4956-ba19-f617085ce97e.png"><br>
-다음과 같이 입력받을 때 font-weight, font-size 등이 수정되도록 바꾸어 주세요
-
+다음과 같이 입력받을 때 font-weight, font-size 등이 수정되도록 바꾸어 주세요  
+sdsdssdsdsdsdfsdfsdfsdsdfsdfsdfssidsdfsdfsdfsdfsdfsdfdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsd
 
 # 실습 2번문제 
  색상코드를 입력하면 색상과 text가 나오는 카드를 만들어봅시다! 제한된 조건 속에서 진행해 주시길 바랍니다!!
@@ -48,7 +48,7 @@ ex) <br>
 
 # 실습 3번문제
  대망의 끝판왕 MediaCard 컴포넌트를 제작합니다!
- 무슨 방법을 사용하셔도 괜찮습니다. 최선을 다해서 사진과 유사한 미디어 카드를 만들어주세요! FE는 필수소양은 디자인을 완벽하게 같게 구사하는 능력입니다.<br>
+ 무슨 방법을 사용하셔도 괜찮습니다. 최선을 다해서 사진과 유사한 미//디어 카드를 만들어주세요! FE는 필수소양은 디자인을 완벽하게 같게 구사하는 능력입니다.<br>
  <img width="668" alt="image" src="https://user-images.githubusercontent.com/77886826/166138976-2db612eb-7b7a-4a7c-882a-3199b9d55ab7.png">
 
 요런 컴포넌트를 만들어 주세요! 
--- src/component/MediaCard/index.js
+++ src/component/MediaCard/index.js
@@ -1,1 +1,62 @@
-// 여기다가 만드시면 됩니다!!!
+import React from "react";
+import styled from "styled-components";
+import TextBox from "../Text";
+
+
+const PhotoWrapper = styled.div`
+  background-image: url(${(props) => (props.src ? props.src : "none")});
+  background-size: cover;
+  border-radius: 20px;
+  height: 200px;
+  //
+`;
+const TextWrapper = styled.div`
+
+margin-top: 10px;
+margin-bottom: 10px;
+  padding: 10px;
+  height: 95px;
+  //
+`;
+const ButtonWrapper = styled.div`
+border-radius: 25px;
+padding: 17px 17px;
+text-align: center;
+background: linear-gradient( to left, rgba(255, 255, 255, 0.1), ${(props) => (props.btncolor ? props.btncolor : "white")});
+
+  //
+`;
+const Main = styled.div`
+padding: 10px;
+  background-color: white;
+  width: 350px;
+  height: 400px;
+  border-radius: 25px;
+  margin-bottom: 10px;
+  //
+`;
+
+const MediaCard = (props) => {
+  return (
+    <>
+    <Main>
+      <PhotoWrapper src={props.src}>
+      </PhotoWrapper>
+      <TextWrapper>
+        <TextBox fontsize="30px" fontweight="800">
+          {props.title}
+        </TextBox>
+        <TextBox fontsize="20px" fontweight="200" color="grey">
+          {props.internal}
+        </TextBox>
+        </TextWrapper>
+      <ButtonWrapper btncolor={props.btncolor}>
+        <TextBox fontsize="20px" color="white">Action</TextBox>
+      </ButtonWrapper>
+    </Main>
+    </>
+  );
+};
+
+export default MediaCard;
+
