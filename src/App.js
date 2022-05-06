f3c83c88069191f8d203718c669c2bcc26085c16
pakxe <pigkill40@naver.com>
2022-05-06 19:56:54 +0900

    f

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import ColorCard from "./component/ColorCard";
+import TextBox from "./component/Text";
+import MediaCard from "./component/MediaCard";
+import GlobalStyle from "./component/GlobalStyle";
+
 function App() {
   return (
     <>
-      <ColorCard color="black" />
+      <GlobalStyle />
+      <MediaCard btncolor="blue" src="img/hi.png" title="Heading" internal="This is a short description about the card."> </MediaCard>
+      <MediaCard btncolor="pink" src="img/hi.png" title="I'm pink" internal="hi"> </MediaCard>
+      <MediaCard btncolor="red" src="img/hi.png" title="hello" internal="hello"> </MediaCard>
+      <MediaCard btncolor="purple" src="img/hi.png" title="hi" internal="wow"> </MediaCard>
+      <MediaCard btncolor="green" src="img/hi.png" title="wow" internal="kuby"> </MediaCard>
+      <img src="img/1.png" />
+      <img src="img/2.png" />
+      <img src="img/3.png" />
+      <img src="img/4.png" />
     </>
   );
 }
--- src/component/Text/index.js
+++ src/component/Text/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledText = styled.div`
-  font-size: 10px;
+
+  font-size: ${(props) => (props.size ? props.size : "50px")};
   color: ${(props) => (props.color ? props.color : "black")};
+  font-weight: ${(props) => (props.weight ? props.weight : "80")};
 `;
 
 const TextBox = (props) => {
-  return <StyledText color={props.color}>{props.children}</StyledText>;
+  return <StyledText color={props.color} size={props.fontsize} weight={props.fontweight}>{props.children}</StyledText>;
 };
 
 export default TextBox;
