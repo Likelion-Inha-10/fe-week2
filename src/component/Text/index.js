c63d5a2f77fbf9530359498a665d3e2c5249b990
pakxe <pigkill40@naver.com>
2022-05-09 22:01:06 +0900

    CLN : Refactor TextBox
    변수명을 규칙에 맞게 수정하였습니다.

--- src/component/Text/index.js
+++ src/component/Text/index.js
@@ -3,13 +3,13 @@ import styled from "styled-components";
 
 const StyledText = styled.div`
 
-  font-size: ${(props) => (props.size ? props.size : "50px")};
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "50px")};
   color: ${(props) => (props.color ? props.color : "black")};
-  font-weight: ${(props) => (props.weight ? props.weight : "80")};
+  font-weight: ${(props) => (props.fontWeight ? props.fontWeight : "80")};
 `;
 
 const TextBox = (props) => {
-  return <StyledText color={props.color} size={props.fontsize} weight={props.fontweight}>{props.children}</StyledText>;
+  return <StyledText color={props.color} fontSize={props.fontSize} fontWeight={props.fontWeight}>{props.children}</StyledText>;
 };
 
 export default TextBox;
